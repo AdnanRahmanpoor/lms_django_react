f00c00ccaa5da533c35ebdc360297abcac48a970
Adnan Rahmanpoor <adnan.rp359@gmail.com>
2024-10-30 01:29:04 +0400

    Created Views for resigter and userprofile

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+User = get_user_model()
+
+# Register a new user
+class RegisterUserView(generics.CreateAPIView):
+    queryset = User.objects.all() # retrieve all User objects
+    serializer_class = UserSerializer # Use User serializer for data conversion
+    permission_classes = [AllowAny] # no authentication required for this view so anyone can register
+
+# Retrieve user profile
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
