b1497011cb0aca2bffd8287878b90ac55f1a4ca3
Adnan Rahmanpoor <adnan.rp359@gmail.com>
2024-10-31 02:17:11 +0400

    Creating Serializer for Lesson and Course - Data Conversion to JSON

---
+++ backend/courses/serializers.py
@@ -0,0 +1,19 @@
+'''
+Creating serializers for Lesson and Course model
+for data conversion into JSON format
+'''
+
+from rest_framework import serializers
+from .models import Course, Lesson
+
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = ['id', 'title', 'content', 'created_at']
+
+class CourseSerializer(serializers.ModelSerializer):
+    lessons = LessonSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = ['id', 'title', 'description', 'instructor', 'created_at', 'lessons']
